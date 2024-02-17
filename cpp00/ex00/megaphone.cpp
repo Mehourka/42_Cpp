3160d9c090a396e525256e5bf3d51c0cb1dee5d5
Karim <k.mehour@gmail.com>
2024-02-17 12:48:06 -0500

    Custom transfomr function

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 #include <string>
+#include <cctype>
+
+void ft_transform(
+	std::string::iterator it,
+	std::string::iterator last,
+	std::string::iterator dest,
+	int (*func)(int))
+{
+	while(it != last)
+	{
+		*dest++ = func(*it++);
+	}
+}
 
 int main(int argc, char **argv)
 {
-    (void) argc;
-    std::string s;
+	(void) argc;
+	(void) argv;
+	std::string s;
 
     if (argc == 1)
     {
-        std::cout <<  "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-        return (0);
+        std::cout <<  "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     }
+	else for(int i = 1; i < argc; i++)
+	{
+		s = argv[i];
+		ft_transform(s.begin(), s.end(), s.begin(), toupper);
+		std::cout << s;
+	}
 
-    for (int i = 1; argv[i]; i++)
-    {
-        s = argv[i];
-        std::transform(s.begin(), s.end(), s.begin(), std::ptr_fun<int, int>(std::toupper));
-        std::cout << s;
-    }
-    std::cout << std::endl;
-
-    return (0);
+	std::cout << std::endl;
+	return (0);
 }
+
