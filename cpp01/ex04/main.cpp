5f8140463673160fa74991c5dd6fc07cb4c38cd1
Karim <k.mehour@gmail.com>
2024-02-25 21:55:04 -0500

    Ex04: Fix eof and new lines

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -5,19 +5,6 @@ void print_usage()
     std::cout << "mySed usage: ./mySed [input_file] [str1] [str2]" << std::endl;
 }
 
-bool ft_replace(string &str, const string target, const string rep)
-{
-    std::size_t pos = str.find(target);
-    if (pos == string::npos)
-    {
-        return false;
-    }
-   
-    str.erase(pos, target.size());
-    str.insert(pos, rep);
-    return true;
-}
-
 void ft_replace_all(string &str, std::size_t start, const string target, const string rep)
 {
     std::size_t pos = str.find(target, start);
@@ -25,11 +12,11 @@ void ft_replace_all(string &str, std::size_t start, const string target, const s
     {
         return;
     }
-   
+
     str.erase(pos, target.size());
     str.insert(pos, rep);
     std::size_t new_start = pos + rep.size();
-    
+
     ft_replace_all(str, new_start, target, rep);
 }
 
@@ -47,11 +34,11 @@ int main(int argc, char **argv)
     string str1 = argv[2];
     string str2 = argv[3];
 
-    // Open a file  
+    // Open a file
     std::ifstream infile(infile_path.data());
     if (!infile.is_open())
     {
-        std::cout << "Cannot Open input file" << std::endl;
+        std::cout << "Cannot Open: " << infile_path << std::endl;
         return (0);
     }
 
@@ -59,14 +46,27 @@ int main(int argc, char **argv)
     string outfile_path = infile_path + ".replace";
     std::ofstream outfile(outfile_path.c_str());
 
-	// Read, replace ans write 
+    if (!outfile.is_open())
+    {
+        std::cout << "Cannot write to: " << outfile_path << std::endl;
+        infile.close();
+        return (0);
+    }
+
+	// Read, replace ans write
     string line;
     while (!infile.eof())
     {
         std::getline(infile, line);
-        ft_replace_all(line,0, str1, str2);    
+        ft_replace_all(line,0, str1, str2);
+        if (!infile.eof())
+        {
+            line += "\n";
+        }
         outfile << line;
     }
 
+    infile.close();
+    outfile.close();
     std::cout << "Operation complete: " << outfile_path << " Created" << std::endl;
 }
