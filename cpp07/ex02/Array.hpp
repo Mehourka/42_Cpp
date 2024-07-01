7ebe43988561fdf20a1174a3a22863cb9b87bf43
Karim <k.mehour@gmail.com>
2024-07-01 00:24:51 -0400

    Add DeepCopy, tests, and fix memory leaks

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,38 +6,40 @@
 template < typename T = char >
 class Array
 {
-public:
+
+private:
   T*        _content;
   unsigned  _size;
 
 public:
-  Array(unsigned size) : _size(size)
-  {
-    _content = new T[_size];
+  Array(unsigned size) : _size(size) {
+    _content = new T[_size]();
   }
-  // Canonical
-  Array() : _size(0)
-  {
-    Log("Default Constructor");
-    _content = new T[_size];
+
+  Array() : _size(0) {
+    _content = new T[_size]();
   }
+
   Array(Array<T> const &A) {
-    Log("Array Copy Constructor");
     *this = A;
   }
+
   ~Array() {
-    Log("Array Destructor");
     delete[] _content;
   }
+
   Array<T> & operator = (const Array<T> &other) {
-    Log("Array equality override");
-    LogErr("Unimplemented Deep Copy"); 
+    this->_size = other.size();
+
     // Deep Copy
+    delete[] this->_content;
+    this->_content = new T[_size]();
+    for (unsigned i = 0; i < _size; i++)
+      (*this)[i] = other[i];
     return *this;
   }
 
-  void check_access(unsigned i) const
-  {
+  void check_access(unsigned i) const {
     if ( i >= _size )
       throw ForbiddenAccess();
   }
@@ -48,15 +50,31 @@ public:
     return this->_content[i];
   }
 
-  T&  operator [] (int i) // Set with bracket
+  T&  operator [] (unsigned i) // Set with bracket
   {
     check_access(i);
     return this->_content[i];
+  }
+
+  unsigned size() const { 
+    return _size; 
   }
    
   class ForbiddenAccess : std::exception
   {
   public:
-    const char* what() const throw() { return "[Error] Invalid Index";}
+    const char* what() const throw() { return "[ForbiddenAccess] Invalid Index";}
   };
 };
+
+
+template < typename T >
+std::ostream & operator << (std::ostream & o, const Array<T> &array)
+{
+  o << "Array: ";
+  for (unsigned i = 0; i < array.size(); ++i)
+    o << array[i] << " ";
+  o << std::endl;
+  return o;
+}
+
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,19 +1,35 @@
 #include "Array.hpp"
 
+void test_copy(const Array<int> &A)
+{
+  Array<int> B(3);
+  std::cout << "Testing deep copy" << std::endl;
+  std::cout << "B - " << B << std::endl;
+  B = A;
+  B[0] = 42;
+  std::cout << "A - " << A << std::endl;
+  std::cout << "B - " << B << std::endl;
+}
+
 int main(void)
 {
-  int size = 5;
-  Array<int> A(size);
-
-  for (int i = 0; i < size; ++i)
-  {
-    A[i] = i * 2;
-    std::cout << A[i] << std::endl;
-  }
-
-  int i = 1;
   try {
-    std::cout << "A[" << i << "] = " << A[i] <<std::endl;
+    int size = 5;
+    Array<int> A(size);
+
+    // Create An array
+    for (int i = 0; i < size; ++i)
+    {
+      A[i] = i * 2;
+    }
+    std::cout << A << std::endl;
+    // Copy an Array
+
+    // Access an element (change index for testing)
+    int index = 1;
+    std::cout << "A[" << index << "] = " << A[index] <<std::endl;
+
+    test_copy(A);
   } catch (const Array<int>::ForbiddenAccess &e) {
     LogErr(e.what()); 
   }
