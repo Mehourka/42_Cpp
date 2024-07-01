5d00157f7b3aa217f5c939cfeac0cb549d0029cf
Karim <k.mehour@gmail.com>
2024-06-30 23:50:23 -0400

    Add array with bracket access

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,3 +1,62 @@
 #pragma once
 
 #include "Common.hpp"
+#include <exception>
+
+template < typename T = char >
+class Array
+{
+public:
+  T*        _content;
+  unsigned  _size;
+
+public:
+  Array(unsigned size) : _size(size)
+  {
+    _content = new T[_size];
+  }
+  // Canonical
+  Array() : _size(0)
+  {
+    Log("Default Constructor");
+    _content = new T[_size];
+  }
+  Array(Array<T> const &A) {
+    Log("Array Copy Constructor");
+    *this = A;
+  }
+  ~Array() {
+    Log("Array Destructor");
+    delete _content;
+  }
+  Array<T> & operator = (const Array<T> &other) {
+    Log("Array equality override");
+    LogErr("Unimplemented Deep Copy"); 
+    // Deep Copy
+    return *this;
+  }
+
+  void check_access(unsigned i) const
+  {
+    if ( i >= _size )
+      throw ForbiddenAccess();
+  }
+
+  T   operator [] (unsigned i) const // Get with bracket
+  {
+    check_access(i);
+    return this->_content[i];
+  }
+
+  T&  operator [] (int i) // Set with bracket
+  {
+    check_access(i);
+    return this->_content[i];
+  }
+   
+  class ForbiddenAccess : std::exception
+  {
+  public:
+    const char* what() const throw() { return "[Error] Invalid Index";}
+  };
+};
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,8 +1,21 @@
-#include "Common.hpp"
 #include "Array.hpp"
-
 
 int main(void)
 {
+  int size = 5;
+  Array<int> A(size);
+
+  for (int i = 0; i < size; ++i)
+  {
+    A[i] = i * 2;
+    std::cout << A[i] << std::endl;
+  }
+
+  int i = 1;
+  try {
+    std::cout << "A[" << i << "] = " << A[i] <<std::endl;
+  } catch (const Array<int>::ForbiddenAccess &e) {
+    LogErr(e.what()); 
+  }
 }
 
