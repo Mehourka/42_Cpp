6331444a2eff72fcff70ce183b735f9fdcb5430b
Karim Mehour <kmehour@c1r6p2.42quebec.com>
2024-07-27 13:22:55 -0400

    implemented requirements

---
+++ cpp08/ex01/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "Span.hpp"
+
+int main2()
+{
+	std::vector<int> input;
+
+	for (int i = 0; i < 0; i++)
+	{
+		input.push_back(i * 3);
+	}
+
+	Span span(6);
+	span.insert_range(input.begin(), input.end());
+	std::cout << span << std::endl;
+
+	Span span2(span);
+	std::cout << span << std::endl;
+
+	return 0;
+}
+
+int main()
+{
+	Span sp = Span(10);
+	sp.addNumber(-100);
+	sp.addNumber(100);
+	sp.addNumber(20);
+
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+
+
+	std::cout << sp << std::endl;
+
+	return 0;
+
+
+}
