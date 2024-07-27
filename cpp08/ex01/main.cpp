b86448cb0ddf9c384a1dd17d8cec3022afe2cd5b
Karim Mehour <kmehour@c1r6p2.42quebec.com>
2024-07-27 13:50:56 -0400

    Add pramga once

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -8,10 +8,10 @@ void test_negative_spans();
 void test_insert_range();
 
 int main() {
-	// test_spans();
+	test_spans();
 	// test_max_size();
 	// test_negative_spans();
-	test_insert_range();
+	// test_insert_range();
 }
 
 void test_spans() {
